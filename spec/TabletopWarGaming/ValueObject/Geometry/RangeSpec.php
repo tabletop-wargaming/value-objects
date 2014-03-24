58b5c969ec424ba590be6b94d42918f745671085
Barney Hanlon <barney@shrikeh.net>
2014-03-24 09:09:25 +0000

    Add to range so that start measurement cannot be larger than end measurement

--- spec/TabletopWarGaming/ValueObject/Geometry/RangeSpec.php
+++ spec/TabletopWarGaming/ValueObject/Geometry/RangeSpec.php
@@ -2,13 +2,37 @@
 
 namespace spec\TabletopWargaming\ValueObject\Geometry;
 
-use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
+use \PhpSpec\ObjectBehavior;
+use \Prophecy\Argument;
+use \TabletopWargaming\ValueObject\Geometry\Measurement;
+use \TabletopWargaming\ValueObject\Geometry\System;
 
 class RangeSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function let($start, $end)
     {
-        $this->shouldHaveType('TabletopWargaming\ValueObject\Geometry\Range');
+        $start->beADoubleOf('TabletopWargaming\ValueObject\Geometry\Measurement');
+        $end->beADoubleOf('TabletopWargaming\ValueObject\Geometry\Measurement');
+        $this->beConstructedWith($start, $end);
+    }
+
+    function it_should_return_the_start_measurement(Measurement $start)
+    {
+        $this->getStart()->shouldReturn($start);
+    }
+
+    function it_should_return_the_end_measurement(Measurement $end)
+    {
+        $this->getEnd()->shouldReturn($end);
+    }
+
+    function it_should_not_allow_a_start_value_higher_than_the_end_value(
+        Measurement $start,
+        Measurement $end
+    )
+    {
+        $start->toBase()->willReturn(20);
+        $end->toBase()->willReturn(19);
+        $this->shouldThrow('\LengthException')->during('__construct', array($start, $end));
     }
 }
--- src/TabletopWarGaming/ValueObject/Geometry/Range.php
+++ src/TabletopWarGaming/ValueObject/Geometry/Range.php
@@ -2,6 +2,30 @@
 
 namespace TabletopWargaming\ValueObject\Geometry;
 
+use \TabletopWargaming\ValueObject\Geometry\Measurement;
+
 class Range
 {
+    private $start;
+
+    private $end;
+
+    public function __construct(Measurement $start, Measurement $end)
+    {
+        if ($start->toBase() > $end->toBase()) {
+            throw new \LengthException('Start of the range cannot be larger than the end');
+        }
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    public function getStart()
+    {
+        return $this->start;
+    }
+
+    public function getEnd()
+    {
+        return $this->end;
+    }
 }
